01643d5aeeeacd953ca2d87458c2cbd520fadd8a
kiarieking <kiariekevin22@gmail.com>
2026-02-09 14:49:38 +0000

    Polished both script files

--- log_filter.py
+++ log_filter.py
@@ -1,19 +1,21 @@
 import re
 from pathlib import Path
 
-LOG_FILE = "/var/log/nginx/access.log"
-OUTPUT_FILE = "/home/kkiarie/code/error_report/error_only.log"
+LOG_FILE = "/var/log/postgresql/postgresql-17-main.log"
+OUTPUT_FILE = "/home/kkiarie/error_report/error_only.log"
 
 ERROR_PATTERNS = [
 
-    r"\bHEAD\b",
-    r"\bCRICTICAL\b",
-    r"\bWARNING\b",
-]
+    r"\bERROR\b",
+    r"\bSTATEMENT\b",
+    
+    ]
 
 def is_error(line: str)->bool:
     for pat in ERROR_PATTERNS:
-        re.search(pat, line, re.IGNORECASE)
+        if re.search(pat, line, re.IGNORECASE):
+            return True
+    return False
 
 
 def filter_errors(log_path:str):
