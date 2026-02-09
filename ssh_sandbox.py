6a370349d2cd7a71eb39ddee582f8aa586e6eb98
kiarieking <kiariekevin22@gmail.com>
2026-02-09 14:52:42 +0000

    Polished both script files

--- ssh_sandbox.py
+++ ssh_sandbox.py
@@ -6,4 +6,4 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh.connect('139.59.111.67', username='kkiarie', password='kiarie1234', timeout=10)
 
-stdin, stdout, stderr = ssh.exec_command('python3 /home/kkiarie/error_report/log_filter.py')
+stdin, stdout, stderr = ssh.exec_command('python3 /home/kkiarie/logs_notifier/log_filter.py')
