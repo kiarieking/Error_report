edbe6d74726fdf2228a91324eee6b7d822de8861
kkiarie <kiariekevin22@gmail.com>
2026-02-09 16:44:07 +0300

    Script to ssh into server - Complete

--- ssh_sandbox.py
+++ ssh_sandbox.py
@@ -4,4 +4,6 @@ ssh = paramiko.SSHClient()
 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh
+ssh.connect('139.59.111.67', username='kkiarie', password='kiarie1234', timeout=10)
+
+stdin, stdout, stderr = ssh.exec_command('python3 /home/kkiarie/error_report/log_filter.py')
