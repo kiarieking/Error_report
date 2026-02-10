ac3da696291ac71a0bd11071de6721343fde576c
kkiarie <kiariekevin22@gmail.com>
2026-02-10 13:04:16 +0300

    Email notification start

--- email_notification.py
+++ email_notification.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 
-sender_email = 'kiariekevin22@gmail.com;
+sender_email = 'kiariekevin22@gmail.com'
 receiver_email = 'kelvin.kiarie@quatrixglobal.com'
 app_password = 'zfyu sang zeuc ycvg'
 
@@ -10,6 +10,12 @@ msg["From"] = sender_email
 msg["To"] = receiver_email
 msg["Subject"] = "Log Alert: Error Detected in Logs"
 msg.set_content("An error has been detected in the logs. Please check the attached log file for details.")
-with open("/home/kkiarie/error_report/error_only.log", "r") as f:
+with open("/home/kkiarie/code/logs_notifier/log_notifier.log", "r") as f:
     log_content = f.read()
 msg.add_attachment(log_content, filename="error_only.log") 
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+    server.login(sender_email, app_password)
+    server.send_message(msg)
+
+print("Email sent successfully!")
