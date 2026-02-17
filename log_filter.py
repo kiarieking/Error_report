3501eb41add6578df47b8f7ceb85a201d7b21592
kiarieking <kiariekevin22@gmail.com>
2026-02-17 10:52:00 +0000

    [ADD] Dotenv for log paths

--- log_filter.py
+++ log_filter.py
@@ -11,18 +11,15 @@ sender_email = os.getenv("SENDER_EMAIL")
 receiver_email = os.getenv("RECIPIENT_EMAIL")
 app_password = os.getenv("APP_PASSWORD")
 
-print(f"Sender Email: {sender_email}")
-print(f"Receiver Email: {receiver_email}")
-print(f"App Password: {app_password}")
-
-LOG_FILE = "/var/log/postgresql/postgresql-17-main.log"
-OUTPUT_FILE = "/home/kkiaries/logs_notifier/log_notifier.log"
+log_paths = [os.getenv("POSTGRES_LOG"),os.getenv("NGINX_LOG"),os.getenv("ODOO_LOG")]
+output_file = os.getenv("OUTPUT_FILE")
 
 ERROR_PATTERNS = [
 
     r"\bERROR\b",
     r"\bstarting PostgreSQL\b",
     r"\bshutting down\b",
+    r"\b404\b",
     
     ]
 
@@ -34,23 +31,25 @@ def is_error(line: str)->bool:
 
 
 def filter_errors(log_path:str):
-    log_path = Path(log_path)
-    today = datetime.utcnow().strftime("%Y-%m-%d")
+    for i in range(len(log_paths)):
 
-    if not log_path.exists():
-        raise FileNotFoundError(f"{log_path} dos not exist")
-    
-    try:
-        with log_path.open("r", errors="ignore") as log, open(OUTPUT_FILE,"w") as out:
-            for line in log:
-                if not line.startswith(today):
-                    continue
-                if is_error(line):
-                    out.write(line)
-    except FileNotFoundError as e:
-        print(f"Error in filepath: {e}")
-    except Exception as e:
-        print (f"General Error: {e}")
+        log_path = Path(log_path)
+        today = datetime.utcnow().strftime("%Y-%m-%d")
+
+        if not log_path.exists():
+            raise FileNotFoundError(f"{log_path} dos not exist")
+        
+        try:
+            with log_path.open("r", errors="ignore") as log, open(output_file,"w") as out:
+                for line in log:
+                    if not line.startswith(today):
+                        continue
+                    if is_error(line):
+                        out.write(line)
+        except FileNotFoundError as e:
+            print(f"Error in filepath: {e}")
+        except Exception as e:
+            print (f"General Error: {e}")
 
 
     print(f"Filtered errors Written to to {OUTPUT_FILE}")
@@ -58,7 +57,7 @@ def filter_errors(log_path:str):
     email_notification = EmailNotifier(sender_email=sender_email,app_password=app_password)
     email_notification.send_email(receiver_email=receiver_email,subject="Log Alert: Error Detected in Logs",
     body="An error has been detected in the logs. Please check the attached log file for details."
-    ,attachment_path=OUTPUT_FILE)
+        ,attachment_path=OUTPUT_FILE)
 
     print("Email sent successfully!")
 
